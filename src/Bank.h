e224fed802a784dd85087be6b3e3e01579c03ac0
melkov2113 <melkov2113@gmail.com>
2025-12-24 18:56:02 +0300

    Add files via upload

--- src/Bank.h
+++ src/Bank.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <vector>
+#include "Account.h"
+
+class Bank {
+public:
+    std::vector<Account> accounts;
+
+    // Метод для создания аккаунта
+    void createAccount(int id, std::string name, double initial) {
+        accounts.emplace_back(id, name, initial);
+    }
+
+    // Метод для поиска аккаунта по ID (возвращает указатель)
+    Account* getAccount(int id) {
+        for (auto& acc : accounts) {
+            if (acc.getId() == id) {
+                return &acc;
+            }
+        }
+        return nullptr; // Если не найден
+    }
+};
