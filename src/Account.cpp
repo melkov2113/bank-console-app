d5b687fdbb06b8cb4ac0eca516c9b8b5a67698c5
melkov2113 <melkov2113@gmail.com>
2025-12-27 13:20:37 +0300

    Update Account.cpp

--- src/Account.cpp
+++ src/Account.cpp
@@ -1,23 +1,35 @@
 #include "Account.h"
 
-Account::Account(int id, const std::string& name, double initial)
-    : id(id), name(name), balance(initial) {
+Account::Account(int id, std::string name, double balance)
+    : id(id), name(name), balance(balance) {
 }
 
 int Account::getId() const { return id; }
-
-std::string Account::getName()  const { return name; }
-
+std::string Account::getName() const { return name; }
 double Account::getBalance() const { return balance; }
 
 void Account::deposit(double amount) {
-    if (amount > 0) balance += amount;
+    if (amount > 0) {
+        balance += amount;
+        std::cout << "Баланс пополнен. Новый баланс: " << balance << std::endl;
+    }
+    else {
+        std::cout << "Сумма должна быть положительной!" << std::endl;
+    }
 }
 
 bool Account::withdraw(double amount) {
-    if (amount > 0 && amount <= balance) {
+    if (amount > 0 && balance >= amount) {
         balance -= amount;
+        std::cout << "Деньги сняты. Новый баланс: " << balance << std::endl;
         return true;
     }
-    return false;
+    else {
+        std::cout << "Недостаточно средств или неверная сумма!" << std::endl;
+        return false;
+    }
+}
+
+void Account::display() const {
+    std::cout << "ID: " << id << " | Имя: " << name << " | Баланс: " << balance << std::endl;
 }
