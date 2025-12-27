06c90f47e69e06e9d0dd3845a69f05f12db093e3
melkov2113 <melkov2113@gmail.com>
2025-12-27 13:21:45 +0300

    Update FileManager.h

--- src/FileManager.h
+++ src/FileManager.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include "Bank.h"
+#include <string>
+
+class FileManager {
+private:
+    std::string filename;
+
+public:
+    FileManager(std::string filename);
+    void saveData(const Bank& bank);
+    void loadData(Bank& bank);
+};
+
+#endif
